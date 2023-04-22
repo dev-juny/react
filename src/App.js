0b3d3d4a9e67e9668444561ea03a9c5a20f4be22
dev-juny <121218647+dev-juny@users.noreply.github.com>
2023-04-22 18:26:59 +0900

    표준 React 프로젝트 분석하기 index.html, App.js, index.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+//App이라는 함수로 정의됨
+//무언가를 반환하는 함수, HTML을 반환함
+//일반적인 javascript구문이 아니지만 가능한 이유는 JSX때문임
 function App() {
   return (
     <div>
@@ -6,4 +9,5 @@ function App() {
   );
 }
 
+//App이라는 이름으로 export함
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
+// 3rd party libary (제3자가 만든 소프트웨어 라이브러리 import)
 import ReactDOM from 'react-dom/client';
 
+//CSS
 import './index.css';
+
+//App으로 랜더링 될 특정 js , import할떄는 .js라는 확장자를 제외 
+//App은 컴포넌트
+//JSX임
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//ReactDOM에서 createRoot 메소드 호출.
+//React를 사용하여 구축할 전체 사용자 인터페이스의 메인 엔트리 포인트 혹운 메인 훅을 생성
+//React로 만들 사용자 인터페이스가 로딩되는 웹페이지의 어느 곳에 위치해야 하는지를 알려줌
+const root = ReactDOM.createRoot(document.getElementById('root')); //index.html의 <div id="root"></div> 임 span이나 p도 가능하지만 div가 기본적임
+
+//root객체에서 render 메소드를 호출하여 React에게 선택된 div에서 무엇이 렌더링 되어야 하는지 알려줌
+//div 내부가 App으로 렌더링 됨
 root.render(<App />);
