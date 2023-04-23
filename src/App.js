4841cf1fbd8e480671e6ad4d6d5d32d5f3652c41
dev-juny <121218647+dev-juny@users.noreply.github.com>
2023-04-23 13:45:16 +0900

    ExpenseDate 컴포넌트 분리하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 //App이라는 함수로 정의됨
 //무언가를 반환하는 함수, HTML을 반환함
 //일반적인 javascript구문이 아니지만 가능한 이유는 JSX때문임
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  //JSX 소스 자바스크립트 XML을 의미함
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,23 @@
+import ExpenseDate from './ExpenseDate';
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+
+
+    /** 여기 있는것은 실제로 HTML 이 아니고
+     * HTML처럼 보이기는 하지만 리액트 팀에서 만든 JSX구문이다.
+     * 결국은 여전히 javascript 파일이다.
+     * 그래서 class="" 가 아닌 className으로 사용한다.
+     */
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
